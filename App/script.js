f021b715452d8292829f1bf37d6d009fc859c066
Prakash Yadav <yadavparkash02@gmail.com>
2023-12-24 00:29:43 +0530

    commit added

--- App/script.js
+++ App/script.js
@@ -40,14 +40,67 @@ function Finished_Sorting() {
 
 function generateBars(n = -1) {
 	bars = [];
+	//initialBarsState = []; 
 	let container = document.getElementById("container");
+	container.style.flexDirection = 'rowp';
 	n = n < 0 ? Math.random() * 20 : n;
 	for (let i = 0; i < n; i++) {
 		bars.push('<div class="bar" id="' + i + '" style="height:' + Math.floor(2 + Math.random() * 98) + '%"></div>');
+;
+
 	}
+	
 	container.innerHTML = bars.join('');
+
 }
 
+let isPaused = false;
+let currentStep = 0;
+let steps = [];
+
+function pause() {
+    isPaused = true;
+}
+
+function resume() {
+    isPaused = false;
+    executeNextStep();
+}
+
+let initialBarsState = [];
+
+    function resetBars() {
+      let container = document.getElementById("container");
+      container.innerHTML = initialBarsState.join('');
+      isPaused = false;
+      currentStep = 0;
+    }
+
+async function SleepWithPause(ms) {
+    while (isPaused) {
+        await new Promise(resolve => setTimeout(resolve, 100));
+    }
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function executeNextStep() {
+    if (currentStep < steps.length) {
+        steps[currentStep]();
+        currentStep++;
+    } else {
+        Finished_Sorting();
+    }
+}
+
+function executePreviousStep() {
+    if (currentStep > 0) {
+        currentStep--;
+        resetBars(); // You need to implement a function to reset the bars to the state before the current step.
+        for (let i = 0; i < currentStep; i++) {
+            steps[i]();
+        }
+    }
+}
 
 function Sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
@@ -64,40 +117,49 @@ function MapRange(value, in_min, in_max, out_min, out_max) {
 
 // SelectionSort() : Implementation of selection sort algorithm. O(n^2) 
 async function SelectionSort() {
-	let delay = Disable_The_Input();
+    let delay = Disable_The_Input();
 
-	let container = document.getElementById("container");
-	for (let i = 0; i < bars.length; i++) {
-		let mn_ind = i;
-		let curr_id = bars[i].split('id="')[1].split('"')[0];
-		document.getElementById(curr_id).style.backgroundColor = selected;
-		let sound = MapRange(document.getElementById(curr_id).style.height.split('%')[0], 2, 100, 500, 1000);
-		beep(100, sound, delay)
-		for (let j = i + 1; j < bars.length; j++) {
-			let nxt_ele = bars[j].split('id="')[1].split('"')[0];
-			document.getElementById(nxt_ele).style.backgroundColor = chng;
-			let a = parseInt(bars[mn_ind].split(/[:%]/)[1]);
-			let b = parseInt(bars[j].split(/[:%]/)[1]);
-			if (a > b) mn_ind = j;
-			await Sleep(delay / 5.0);
-			document.getElementById(nxt_ele).style.backgroundColor = def;
-		}
+    let container = document.getElementById("container");
+    for (let i = 0; i < bars.length; i++) {
+        let mn_ind = i;
+        let curr_id = bars[i].split('id="')[1].split('"')[0];
+        document.getElementById(curr_id).style.backgroundColor = selected;
+        let sound = MapRange(document.getElementById(curr_id).style.height.split('%')[0], 2, 100, 500, 1000);
+        beep(100, sound, delay);
+        await SleepWithPause(delay / 5.0);
+        for (let j = i + 1; j < bars.length; j++) {
+            let nxt_ele = bars[j].split('id="')[1].split('"')[0];
+            document.getElementById(nxt_ele).style.backgroundColor = chng;
+            let a = parseInt(bars[mn_ind].split(/[:%]/)[1]);
+            let b = parseInt(bars[j].split(/[:%]/)[1]);
+            if (a > b) mn_ind = j;
+            await SleepWithPause(delay / 5.0);
+            document.getElementById(nxt_ele).style.backgroundColor = def;
+        }
 
-		let nxt_ele = bars[mn_ind].split('id="')[1].split('"')[0];
-		document.getElementById(nxt_ele).style.backgroundColor = selected;
-		await Sleep(2 * delay / 5.0);
+        let nxt_ele = bars[mn_ind].split('id="')[1].split('"')[0];
+        document.getElementById(nxt_ele).style.backgroundColor = selected;
+        await SleepWithPause(2 * delay / 5.0);
 
-		let tmp = bars[mn_ind];
-		bars[mn_ind] = bars[i];
-		bars[i] = tmp;
+        let tmp = bars[mn_ind];
+        bars[mn_ind] = bars[i];
+        bars[i] = tmp;
 
-		container.innerHTML = bars.join('');
-		await Sleep(2 * delay / 5.0);
-		document.getElementById(curr_id).style.backgroundColor = def;
-		document.getElementById(nxt_ele).style.backgroundColor = def;
-	}
-	Finished_Sorting();
+        container.innerHTML = bars.join('');
+        await SleepWithPause(2 * delay / 5.0);
+        document.getElementById(curr_id).style.backgroundColor = def;
+        document.getElementById(nxt_ele).style.backgroundColor = def;
+
+        steps.push(() => {
+            // This function represents the state of bars at this step
+            container.innerHTML = bars.join('');
+            document.getElementById(curr_id).style.backgroundColor = def;
+            document.getElementById(nxt_ele).style.backgroundColor = def;
+        });
+    }
+    Finished_Sorting();
 }
+
 
 
 
